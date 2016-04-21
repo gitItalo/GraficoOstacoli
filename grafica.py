26aef39b6158d47dd1735ce1cd2fc400b3e863e2
Stefand5 <sduras27@gmail.com>
2016-04-21 11:55:33 +0200

    Added files via upload
    Grafica.py aggiornata

--- grafica.py
+++ grafica.py
@@ -9,13 +9,16 @@ size = (width, height)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Gioco!')
 clock = pygame.time.Clock()
+
+i = 360
+z=360
 #======================================
 verticeSx1=[500,490]
 verticeDx1=[200,20]
     
 verticeSx2=[680,490]
 verticeDx2=[150,20]
-def grafica(p,c):
+def grafica():
     global verticeSx1, verticeDx2,verticeDx1,verticeSx2
     
     pygame.draw.rect(screen,(255,255,0),(verticeSx1[0],verticeSx1[1],verticeDx1[0],verticeDx1[1]),2)
@@ -24,43 +27,59 @@ def grafica(p,c):
     pygame.draw.circle(screen,(255,255,255),(510,500),320,2)
     pygame.draw.circle(screen,(255,255,255),(510,500),2,2)
     
+    
 def ostacoli():
     pygame.draw.polygon(screen,(0,255,255),[[400,300],[400,350],[350,325]],2)
     pygame.draw.polygon(screen,(0,255,255),[[200,800],[200,850],[150,825]],2)
     pygame.draw.rect(screen,(255,232,76),(800,0,200,1000),2)
-    
 def XYCirconference(center, radius, angle):
-    
+    global xcenter
     rad = math.radians(angle)
     xcenter = center[0]; ycenter = center[1]
     x = xcenter + radius * math.cos(rad)
     y = ycenter + radius * math.sin(rad)
     return [x,y]
 def movimento():
-    pass
+    global z,i,c,p;
+    z -= 1
+    if z == 0:
+        i -= 1
+        z = 360
+    p = XYCirconference([510,500],190,i)
+    c =XYCirconference([p[0],p[1]],130,z)
+def braccio1():
+        z=360
+        i = 360
+        z -= 1
+        if z == 0:
+           i -= 1
+           z = 360
+        p = XYCirconference([510,500],190,i)
+        c = XYCirconference([p[0],p[1]],130,z)
+        #pygame.draw.circle(screen,(255,255,255),(490,500),10,2)
+        pygame.draw.rect(screen,(255,55,44),(int(p[0]),int(p[1]),200,20),1)    
         
 #======================================
 
 
 
+
+u=0
 done = False
 try:
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
+        screen.fill((0,0,0))                      
         
-        for i in range(361):
-            p = XYCirconference([500,500],190,i)
-            for z in range(361):
-                c = XYCirconference([p[0],p[1]],140,z)
-                pygame.draw.circle(screen,(255,25,25),(int(c[0]),int(c[1])),50)
-           
-        grafica(p,c)
+
         movimento()
+        grafica()
+        
+        braccio1()
         ostacoli()
         pygame.display.update()
-        screen.fill((0,0,0))
-        clock.tick(60)
+        clock.tick(300)
 finally:
     pygame.quit()
